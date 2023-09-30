2d33035f72b5caa994532340cd8c6fd9d704fa64
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-09-29 23:44:10 -0500

    setup ssr stuff

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,19 +1,24 @@
 import path from "path";
-
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
-
 import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solid(), sitemap()],
-	vite: {
-		build: { sourcemap: true },
-		resolve: {
-			alias: {
-				"~": path.resolve("./src")
-			}
-		},
-	}
+  site: "https://taolib.vercel.app/",
+  integrations: [solid(), sitemap()],
+  output: "hybrid",
+  vite: {
+    build: {
+      sourcemap: true
+    },
+    resolve: {
+      alias: {
+        "~": path.resolve("./src")
+      }
+    }
+  },
+  adapter: vercel()
 });
